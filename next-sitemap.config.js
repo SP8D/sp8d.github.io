bc2927a365a246ce1752a2b7f601758cb60d82e2
Przemek <przemyslaw.kalka@gmail.com>
2025-05-19 18:14:55 +0100

    Add next-sitemap configuration and update package dependencies; remove old sitemap and robots.txt files

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: "https://sp8d.github.io",
+  generateIndexSitemap: false,
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  outDir: "out",
+};
